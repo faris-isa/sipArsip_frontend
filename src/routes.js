3af983f1c8e7836c4537e1464155fc35c4cb0934
Faris Isa <m.faris.isa@gmail.com>
2020-10-21 10:18:16 +0700

    add routes products

--- src/routes.js
+++ src/routes.js
@@ -37,6 +37,8 @@ const Typography = React.lazy(() => import('./views/theme/typography/Typography'
 const Widgets = React.lazy(() => import('./views/widgets/Widgets'));
 const Users = React.lazy(() => import('./views/users/Users'));
 const User = React.lazy(() => import('./views/users/User'));
+const Products = React.lazy(() => import('./views/products/Products'));
+const AddProducts = React.lazy(() => import('./views/products/AddProducts'));
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
@@ -78,7 +80,9 @@ const routes = [
   { path: '/notifications/toaster', name: 'Toaster', component: Toaster },
   { path: '/widgets', name: 'Widgets', component: Widgets },
   { path: '/users', exact: true,  name: 'Users', component: Users },
-  { path: '/users/:id', exact: true, name: 'User Details', component: User }
+  { path: '/users/:id', exact: true, name: 'User Details', component: User },
+  { path: '/produk', exact: true, name: 'Produk', component: Products },
+  { path: '/produk/tambah', name: 'Tambah', component: AddProducts},
 ];
 
 export default routes;
