c112d78e5a87505de3bb3de4081f971198b0953b
Faris Isa <m.faris.isa@gmail.com>
2021-05-07 15:09:52 +0700

    axios for build prod

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 let api = axios.create({
-    baseURL: "http://localhost/backend/api",
-    // baseURL: "https://m3117063.api.isabot.site/api",
+    // baseURL: "http://localhost/backend/api",
+    baseURL: "https://m3117063.api.isabot.site/api",
     withCredentials: true,
 });
 
